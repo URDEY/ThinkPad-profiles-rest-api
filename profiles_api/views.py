faa9f463574dbecd47cf70ccb65d55818e9a34b5
Udipta <udiptad6@gmail.com>
2021-06-01 06:33:20 +1000

    Download from GITHUB

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import viewsets
-from profiles_api import serializers
+
+from profiles_api import  serializers
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -10,17 +11,14 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-
         an_apiview = [
-            'Uses HTTP methods as functions (get, post, patch, put, delete)',
-            'Is similar to a traditional Django View',
-            'Gives you the most control over your logic',
-            'Is mapped manually to URLs',
+        'Uses HTTP methods as function (get, post, patch, put, delete)',
+        'Is similar to a traditional Django View',
+        'Gives you the most control over you application logic',
+        'Is mapped manually to URLs',
         ]
 
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
-
-
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
 
     def post(self, request):
         """Create a hello message with our name"""
@@ -36,32 +34,17 @@ class HelloApiView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-def put(self, request, pk=None):
-    """Handle updating an object"""
+    def put(self, request, pk=None):
+     """Handle partial update of object"""
 
-    return Response({'method': 'PUT'})
+     return Response({'method': 'PUT'})
 
-def patch(self, request, pk=None):
-    """Handle partial update of object"""
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
 
-    return Response({'method': 'PATCH'})
+        return Response({'method': 'PATCH'})
 
-def delete(self, request, pk=None):
-    """Delete an object"""
+    def delete(self, request, pk=None):
+        """Delete an object"""
 
-    return Response({'method': 'DELETE'})
-
-
-class HelloViewSet(viewsets.ViewSet):
-    """Test API ViewSet"""
-
-    def list(self, request):
-        """Return a hello message."""
-
-        a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update)',
-            'Automatically maps to URLS using Routers',
-            'Provides more functionality with less code',
-        ]
-
-        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+        return Response({'method': 'DELETE'})
